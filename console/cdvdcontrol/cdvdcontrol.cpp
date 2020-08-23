882995a3153275bc24173eb233def8fe87cedf99
Jeremiah Wilson <jeremiahwilson777@gmail.com>
2020-08-22 23:47:48 -0700

    Fix some more clang-tidy warnings

--- console/cdvdcontrol/cdvdcontrol.cpp
+++ console/cdvdcontrol/cdvdcontrol.cpp
@@ -90,19 +90,15 @@ auto get_device_info(drive_info *dev) -> int {
   if (isPlextor(dev) != 0) {
     plextor_get_life(dev);
     if (dev->life.ok) {
-      printf("Discs loaded: %6d\n", dev->life.dn);
+      cout << "Discs loaded: " << dev->life.dn << "\n";
       cout << "Drive operating time:\n";
-      printf("  CD Rd  : %4d:%02d:%02d\n", dev->life.cr.h, dev->life.cr.m,
-             dev->life.cr.s);
-      printf("  CD Wr  : %4d:%02d:%02d\n", dev->life.cw.h, dev->life.cw.m,
-             dev->life.cw.s);
+      cout << "  CD Rd  : " << dev->life.cr.h << ":" << dev->life.cr.m << ":" << dev->life.cr.s << "\n";
+      cout << "  CD Wr  : " << dev->life.cw.h << ":" << dev->life.cw.m << ":" << dev->life.cw.s << "\n";
       if ((dev->rd_capabilities & DEVICE_DVD) != 0U) {
-        printf("  DVD Rd : %4d:%02d:%02d\n", dev->life.dr.h, dev->life.dr.m,
-               dev->life.dr.s);
+        cout << "  DVD Rd  : " << dev->life.dr.h << ":" << dev->life.dr.m << ":" << dev->life.dr.s << "\n";
       }
       if ((dev->wr_capabilities & DEVICE_DVD) != 0U) {
-        printf("  DVD Wr : %4d:%02d:%02d\n", dev->life.dw.h, dev->life.dw.m,
-               dev->life.dw.s);
+        cout << "  DVD Wr  : " << dev->life.dw.h << ":" << dev->life.dw.m << ":" << dev->life.dw.s << "\n";
       }
     }
 
@@ -187,10 +183,9 @@ auto get_device_info(drive_info *dev) -> int {
   if ((flags & FL_SUPPORTED) != 0U) {
     //      printf("____________________________\n");
     cout << "\n** Supported features:\n";
-    printf("AudioMaster Q.R.    : %s\n",
-           dev->ven_features & YMH_AMQR ? "YES" : "-");
+    cout << "AudioMaster Q.R.    : " << (dev->ven_features & YMH_AMQR) != 0U ? "YES" : "-" << "\n";
     printf("Yamaha ForceSpeed   : %s\n",
-           dev->ven_features & YMH_FORCESPEED ? "YES" : "-");
+           (dev->ven_features & YMH_FORCESPEED) != 0U ? "YES" : "-");
     printf("Yamaha DiscT@2      : %s\n",
            dev->ven_features & YMH_TATTOO ? "YES" : "-");
     printf("Hide CD-R           : %s\n",
