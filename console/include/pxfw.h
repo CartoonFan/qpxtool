18be12378b140f7316977ac9838c7da15c993f83
Restyled.io <commits@restyled.io>
2020-08-28 06:58:39 +0000

    Restyled by astyle

--- console/include/pxfw.h
+++ console/include/pxfw.h
@@ -1,7 +1,7 @@
 /*
  * This file is a part of QPxTool project
  * Copyright (C) 2006-2009, Gennady "ShultZ" Kozlov <qpxtool@mail.ru>
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
